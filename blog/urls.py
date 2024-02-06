37a86f50afc042c743c9d7325590839ce33012f6
osemenjoy <osemenjoy45@gmail.com>
2024-02-06 19:34:29 +0100

    my first commit

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls  import path
+from .views import HomePage, PostDetails
+
+urlpatterns = [
+    path('', HomePage.as_view(), name= 'home'),
+    path('post/<int:pk>/', PostDetails.as_view(), name= 'post_detail'),
+]
