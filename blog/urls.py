97771329fcf56e40cc39dcc32d5d1b4d5495e53e
osemenjoy <osemenjoy45@gmail.com>
2024-02-07 11:52:56 +0100

    Create, edit and delete the posts now

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls  import path
-from .views import HomePage, PostDetails
+from .views import HomePage, PostDetails, CreatePost,EditPost, DeletePost
 
 urlpatterns = [
     path('', HomePage.as_view(), name= 'home'),
     path('post/<int:pk>/', PostDetails.as_view(), name= 'post_detail'),
+    path('post/new/', CreatePost.as_view(), name='new_post'),
+    path('post/<int:pk>/edits', EditPost.as_view(), name= 'edit_post'),
+    path('post/<int:pk>/delete', DeletePost.as_view(), name= 'delete_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from  .models import Post
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 class HomePage(ListView):
     model = Post
@@ -10,3 +12,18 @@ class HomePage(ListView):
 class PostDetails(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class CreatePost(CreateView):
+    model = Post
+    template_name ='new_post.html'
+    fields = ['title', 'author', 'body']
+
+class EditPost(UpdateView):
+    model = Post
+    template_name = 'edit_post.html'
+    fields = ['title', 'body']
+
+class DeletePost(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
